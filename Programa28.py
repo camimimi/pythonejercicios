2875417cf0df1c305a8cb65707ed1604e3a654be
camimimi <109166936+camimimi@users.noreply.github.com>
2022-08-09 13:37:48 -0600

    Create Programa28.py

---
+++ Programa28.py
@@ -0,0 +1,8 @@
+#Le saca el área al ancho y largo que indiquemos
+
+x=int(input("Ingrese el Largo"))
+y=int(input("Ingrese el Ancho"))
+def area(valor1, valor2):
+ resultado=x*y
+ print(resultado)
+area(x, y)
