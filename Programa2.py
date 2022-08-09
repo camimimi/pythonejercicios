b46d096b43b454c7659c1cee56a16384a12127f9
camimimi <109166936+camimimi@users.noreply.github.com>
2022-08-09 12:48:12 -0600

    Create Programa2.py

---
+++ Programa2.py
@@ -0,0 +1,14 @@
+#Identifica si es par o impar
+
+n1=int(input("ingrese un numero"))
+res=n1%2
+if res== 0:
+  print("el numero que ingreso es par")
+else:
+    print("el numero que ingreso es impar")
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Sec
