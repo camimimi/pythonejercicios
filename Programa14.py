27b63a1f8c723a88fc0a6f343a62a50a83fbbab0
camimimi <109166936+camimimi@users.noreply.github.com>
2022-08-09 13:16:18 -0600

    Create Programa14.py

---
+++ Programa14.py
@@ -0,0 +1,15 @@
+#Saber si un año es bisiesto o no 
+
+n1=int(input("Ingrese un año "))
+
+if n1/4:
+  if n1/100:
+    if n1/400:
+     print ("El año es bisiesto")
+else: 
+  print ("El año no es bisiesto")
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
