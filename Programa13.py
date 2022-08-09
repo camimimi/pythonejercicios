a9d9363ed49e80b42f25dbbbde6709b2397b5923
camimimi <109166936+camimimi@users.noreply.github.com>
2022-08-09 13:15:09 -0600

    Create Programa13.py

---
+++ Programa13.py
@@ -0,0 +1,14 @@
+#Identificar vocales
+
+n1=str(input("Escriba una letra "))
+
+if n1== "a":
+  print ("Es vocal")
+elif n1== "e":
+  print ("Es vocal")
+elif n1== "i":
+  print ("Es vocal")
+elif n1== "o":
+  print ("Es vocal")
+elif n1== "u":
+  print ("Es vocal")
