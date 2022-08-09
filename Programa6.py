0a71bc9e514284481e20ad8da9f9d6fd301e88ea
camimimi <109166936+camimimi@users.noreply.github.com>
2022-08-09 12:54:55 -0600

    Create Programa6.py

---
+++ Programa6.py
@@ -0,0 +1,9 @@
+#Averiguar cual de los dos numeros es menor
+
+n1=int(input("ingrese el primer número"))
+n2=int(input("ingrese el segundo número"))
+
+if n1<n2:
+  print ("El primer número es menor")
+else:
+  print ("El segundo número es menor")
