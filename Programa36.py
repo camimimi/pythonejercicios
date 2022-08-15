3f8485e1a8206cd1ec45f615658a498e9b44945f
camimimi <109166936+camimimi@users.noreply.github.com>
2022-08-15 11:55:32 -0600

    Create Programa36.py

---
+++ Programa36.py
@@ -0,0 +1,19 @@
+#Deberá de solicitar al usuario 10 números enteros. Debe mostrar la suma entre el primer y el último número de la lista y debe mostrar el resultado de la suma del segundo al noveno número de la lista.
+
+#Pedimos un número entero
+n1=int(input("ingrese un número "))
+n2=int(input("ingrese un número "))
+n3=int(input("ingrese un número "))
+n4=int(input("ingrese un número "))
+n5=int(input("ingrese un número "))
+n6=int(input("ingrese un número "))
+n7=int(input("ingrese un número "))
+n8=int(input("ingrese un número "))
+n9=int(input("ingrese un número "))
+n10=int(input("ingrese un número "))
+#Sumamos el primer y ultimo numero
+r1 = n1+n10
+print ("La primera suma es:",r1)
+#Sumamos los demás números restantes
+r2 = n2+n3+n4+n5+n6+n7+n8+n9
+print ("La segunda suma es:",r2)
